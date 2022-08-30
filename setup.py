615c0437f0e61d578459b0c60ed920369a340cfa
Seth Onyango <sethi.onyango@gmail.com>
2022-08-30 18:29:41 +0300

    Add .gitignore

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     license='MIT',
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     packages=find_packages(exclude=["tests", "tests.*"]),
-    install_requires=['mpi4py>=2.0', 'numpy', ],
+    install_requires=['', '', ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
