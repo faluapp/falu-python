035a9785177a5da345e1a26b5962d4026280e31a
Seth Onyango <sethi.onyango@gmail.com>
2023-02-15 10:28:48 +0300

    Build request headers and execute api requests

---
+++ falu/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+
+# Bindings for Falu
+# API docs at https://falu.io/docs/api
+
+api_key = None
+app_information = None
+
+# API services
+from falu.services import *
---
+++ falu/client/api_client.py
@@ -0,0 +1,68 @@
+import requests
+from requests import Response
+
+from falu.client.falu_model import FaluModel
+from falu.client.falu_model import deserialize_falu_response
+
+
+class ApiClient(FaluModel):
+    def __init__(self):
+        super().__init__()
+        self.base_url = 'https://api.falu.io/v1'
+
+    @classmethod
+    def _execute(cls, method, path, data=None, key=None, idempotency_key: str = None, workspace=None, live: bool = None,
+                 params=None):
+        client = ApiClient()
+        return client.execute(method, path, data, key, idempotency_key, workspace, live, params)
+
+    def execute(self, method, path, data=None, key=None, idempotency_key: str = None, workspace=None, live: bool = None,
+                params=None):
+        url = self._build_url(path)
+        headers = self.build_headers(key=key, idempotency_key=idempotency_key, workspace=workspace, live=live)
+
+        response = requests.request(method=method, url=url, headers=headers, data=data, params=params)
+        return self.response_handler(response)
+
+    def _build_url(self, path):
+        return "%s%s" % (self.base_url, path)
+
+    @staticmethod
+    def build_headers(key=None, idempotency_key: str = None, workspace: str = None, live: bool = None):
+        if key:
+            api_key = key
+        else:
+            from falu import api_key
+            api_key = api_key
+
+        if api_key is None:
+            pass
+            # TODO: raise error
+
+        headers = {
+            "Authorization": "Bearer %s" % api_key,
+            "X-Falu-Version": "2022-05-01"
+        }
+
+        if idempotency_key is not None:
+            headers['X-Idempotency-Key'] = idempotency_key
+
+        if workspace is not None:
+            headers['X-Workspace-Id'] = workspace
+
+        live = live is not None and live
+        headers['X-Live-Mode'] = str(live)
+        return headers
+
+    def response_handler(self, response: Response):
+        code = response.status_code
+        resource = None
+
+        if response:
+            resource = response.json()
+
+        return self.deserialize_response(code, response.headers, resource)
+
+    @staticmethod
+    def deserialize_response(code, headers, resource):
+        return deserialize_falu_response(code, headers, resource)
