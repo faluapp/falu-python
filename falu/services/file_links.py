a9a180cd9ec0b397b1b60dc303fe8fc69eab3eec
Seth Onyango <sethi.onyango@gmail.com>
2023-03-17 10:00:45 +0300

    Add endpoints for file links service

---
+++ falu/services/file_links.py
@@ -0,0 +1,83 @@
+from falu.generic.get_api_request import GetApiRequest
+from falu.generic.post_api_request import PostApiRequest
+
+
+class FileLink(PostApiRequest, GetApiRequest):
+    """
+    To share the contents of an File object with non-Falu users, you can create an FileLink.
+    It contains a URL that can be used to retrieve the contents of the file without authentication.
+    """
+
+    @classmethod
+    def get_file_links(cls, api_key=None, idempotency_key: str = None, workspace=None, live: bool = None):
+        """
+        Get file links
+
+        :param api_key:
+        :param idempotency_key:
+        :param workspace:
+        :param live:
+        :return:
+        """
+
+        return cls.get(
+            path="/file_links",
+            api_key=api_key,
+            idempotency_key=idempotency_key,
+            workspace=workspace,
+            live=live)
+
+    @classmethod
+    def create_file_links(cls, data, api_key=None, idempotency_key: str = None, workspace=None, live: bool = None):
+        """
+        Create file links
+
+        :param data:
+        :param api_key:
+        :param idempotency_key:
+        :param workspace:
+        :param live:
+        :return:
+        """
+
+        return cls.create(
+            path="/file_links",
+            data=data,
+            api_key=api_key,
+            idempotency_key=idempotency_key,
+            workspace=workspace,
+            live=live)
+
+    @classmethod
+    def get_file_link(cls, link, api_key=None, idempotency_key: str = None, workspace=None, live: bool = None):
+        """
+        Get file links
+
+        :param link:
+        :param api_key:
+        :param idempotency_key:
+        :param workspace:
+        :param live:
+        :return:
+        """
+
+        return cls.create(
+            path="/file_links/{link}".format(link=link),
+            api_key=api_key,
+            idempotency_key=idempotency_key,
+            workspace=workspace,
+            live=live)
+
+    @classmethod
+    def update_file_link(cls, link, api_key=None, idempotency_key: str = None, workspace=None, live: bool = None):
+        """
+        Update file links
+
+        :param link:
+        :param api_key:
+        :param idempotency_key:
+        :param workspace:
+        :param live:
+        :return:
+        """
+        pass
