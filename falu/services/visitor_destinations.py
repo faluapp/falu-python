244bf37e0764441333093de4e7b46718760ee3e5
Seth Onyango <sethi.onyango@gmail.com>
2023-04-11 07:34:48 +0300

    Add tests for visit destination service

--- falu/services/visitor_destinations.py
+++ falu/services/visitor_destinations.py
@@ -42,7 +42,7 @@ class VisitorDestination(PostApiRequest, GetApiRequest, DeleteApiRequest):
 
         return cls.create(
             path="/visits/destinations",
-            data=data,
+            data=cls.serialize(data),
             api_key=api_key,
             idempotency_key=idempotency_key,
             workspace=workspace,
---
+++ tests/services/visitor_destination_tests.py
@@ -0,0 +1,78 @@
+import unittest
+
+import responses
+from responses import matchers
+
+import falu
+
+
+class VisitorDestinationTests(unittest.TestCase):
+    base_url = "https://api.falu.io/v1"
+
+    destination = {
+        "name": "KIMANI AND CO. ADVOCATES (Suite 3B)",
+        "enabled": True,
+        "metadata": {
+            "property_1": "string",
+            "property_2": "string"
+        },
+        "id": "vdst_602a8dd0a54847479a874de4",
+        "created": "2023-04-06T13:16:22Z",
+        "updated": "2023-04-06T13:16:22Z",
+        "workspace": "wksp_602a8dd0a54847479a874de4",
+        "live": True,
+        "etag": "FL9rRnlW2Qg="
+    }
+
+    @responses.activate
+    def test_getting_visitor_destinations_works(self):
+        resp = responses.get("{}/visits/destinations".format(self.base_url), json=[self.destination],
+                             status=200)
+        responses.add(resp)
+
+        resources = falu.VisitorDestination.get_destinations()
+
+        self.assertIsNotNone(resources)
+        self.assertEqual(200, resp.status)
+
+    @responses.activate
+    def test_getting_visitor_destination_works(self):
+        resp = responses.get("{}/visits/destinations/{}".format(self.base_url, "vdst_602a8dd0a54847479a874de4"),
+                             json=self.destination, status=200)
+        responses.add(resp)
+
+        resources = falu.VisitorDestination.get_destination(destination="vdst_602a8dd0a54847479a874de4")
+
+        self.assertIsNotNone(resources)
+        self.assertEqual(200, resp.status)
+
+    @responses.activate
+    def test_adding_visitor_destination_works(self):
+        request = {
+            "name": "KIMANI AND CO. ADVOCATES (Suite 3B)",
+            "enabled": True,
+        }
+
+        resp = responses.post(
+            "{}/visits/destinations".format(self.base_url),
+            json=self.destination,
+            match=[matchers.json_params_matcher(request)],
+            status=200
+        )
+        responses.add(resp)
+
+        resource = falu.VisitorDestination.create_destination(data=request)
+
+        self.assertIsNotNone(resource)
+        self.assertEqual(200, resp.status)
+
+    @responses.activate
+    def test_deleting_visitor_destination_works(self):
+        resp = responses.delete("{}/visits/destinations/{}".format(self.base_url, "vdst_602a8dd0a54847479a874de4"),
+                                json=self.destination, status=200)
+        responses.add(resp)
+
+        resource = falu.VisitorDestination.delete_destination(destination="vdst_602a8dd0a54847479a874de4")
+
+        self.assertIsNotNone(resource)
+        self.assertEqual(200, resp.status)
